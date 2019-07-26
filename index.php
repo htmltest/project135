ec66b7c7c707e0a59e330ab77340a6c2d6ced4df
htmltest <struev.vs@gmail.com>
2019-07-26 16:31:36 +0300

    first

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+    require_once 'vendor/autoload.php';
+
+    $defaultConfig = (new Mpdf\Config\ConfigVariables())->getDefaults();
+    $fontDirs = $defaultConfig['fontDir'];
+
+    $defaultFontConfig = (new Mpdf\Config\FontVariables())->getDefaults();
+    $fontData = $defaultFontConfig['fontdata'];
+
+    $mpdf = new \Mpdf\Mpdf([
+        'fontDir' => array_merge($fontDirs, [
+            'fonts',
+        ]),
+        'fontdata' => $fontData + [
+            'roboto' => [
+                'R' => 'Roboto-Regular.ttf',
+                'B' => 'Roboto-Bold.ttf',
+                'I' => 'Roboto-Medium.ttf',
+            ]
+        ],
+        'default_font' => 'roboto',
+        'format' => 'A4'
+    ]);
+
+    $stylesheet = file_get_contents('style.css');
+    $html = file_get_contents('index.html');
+
+    $mpdf->SetTitle('НЦМУ | Московский центр математических исследований');
+
+    $mpdf->WriteHTML($stylesheet,\Mpdf\HTMLParserMode::HEADER_CSS);
+    $mpdf->WriteHTML($html,\Mpdf\HTMLParserMode::HTML_BODY);
+
+    $mpdf->Output();
+?>
