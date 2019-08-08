cab6702f5bcd092c96f7b2fc6bc94703b89bd6ad
htmltest <struev.vs@gmail.com>
2019-08-08 16:46:21 +0300

    fix pdf 1

--- index.php
+++ index.php
@@ -19,13 +19,43 @@
             ]
         ],
         'default_font' => 'roboto',
-        'format' => 'A4'
+        'format' => 'A3',
+        'setAutoTopMargin' => 'pad',
+        'setAutoBottomMargin' => 'pad'
     ]);
 
     $stylesheet = file_get_contents('style.css');
     $html = file_get_contents('index.html');
 
     $mpdf->SetTitle('НЦМУ | Московский центр математических исследований');
+
+    $mpdf->SetHTMLHeader('<div class="header">
+            <table>
+                <tbody>
+                    <tr>
+                        <td class="logo"><img src="logo.png" alt="" width="218" /></td>
+                        <td class="header-contacts">
+                            <div class="phone">+7 (495) 916-81-08</div>
+                            <div class="email">ncmu@riep.ru</div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>');
+
+    $mpdf->SetHTMLFooter('<div class="footer">
+            <table>
+                <tbody>
+                    <tr>
+                        <td class="logo"><img src="logo.png" alt="" width="218" /></td>
+                        <td class="footer-contacts">
+                            <div class="phone">+7 (495) 916-81-08</div>
+                            <div class="email">ncmu@riep.ru</div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>');
 
     $mpdf->WriteHTML($stylesheet,\Mpdf\HTMLParserMode::HEADER_CSS);
     $mpdf->WriteHTML($html,\Mpdf\HTMLParserMode::HTML_BODY);
